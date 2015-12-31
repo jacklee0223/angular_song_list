77f4b5b91e01bbffa11a91597971e322c589eabe
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-12-31 00:32:48 -0800

    changed .listen to configure on Heroku

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.use(express.static(__dirname + '/public'));
  });
 
  // start the server on port 3000 (http://localhost:3000)
- app.listen(3000);
+ app.listen(process.env.PORT || 3000);
  console.log('Magic happens on port 3000.');
